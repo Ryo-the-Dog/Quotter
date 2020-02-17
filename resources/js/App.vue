2ed1f82cc92e0dd70672b7b7511d72f9d4b14440
Ryo-the-Dog <jrt_ryo@yahoo.co.jp>
2020-02-17 21:25:21 +0900

    Candyの(4)の途中。 会員登録機能実装の直前。 マイグレート未。

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <main>
+            <div class="container">
+                <!-- <RouterView /> の部分に、URL に対応する HTML 部品が入れ替わって描画されます。 -->
+                <RouterView />
+            </div>
+        </main>
+    </div>
+</template>
