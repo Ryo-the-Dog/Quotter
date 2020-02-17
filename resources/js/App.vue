8ac94d1af749669f584da2fa5493b88cd4187175
Ryo-the-Dog <jrt_ryo@yahoo.co.jp>
2020-02-17 23:41:34 +0900

    ヘッダーとフッターを追加しました。

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,29 @@
 <template>
     <div>
+        <header>
+            <Navbar />
+        </header>
+
         <main>
             <div class="container">
                 <!-- <RouterView /> の部分に、URL に対応する HTML 部品が入れ替わって描画されます。 -->
                 <RouterView />
             </div>
         </main>
+
+        <Footer />
+
     </div>
 </template>
+
+<script>
+    import Navbar from './components/Navbar.vue'
+    import Footer from './components/Footer.vue'
+
+    export default {
+        components: {
+            Navbar,
+            Footer
+        }
+    }
+</script>
