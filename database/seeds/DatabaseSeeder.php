24a4969ac9f382505d6071e093ae2b6582eca16a
Ryo-the-Dog <jrt_ryo@yahoo.co.jp>
2020-05-26 15:55:24 +0900

    seederファイルを修正しました。

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(TagTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-
+        $this->call(PhrasesTableSeeder::class);
+        $this->call(Phrase_tagTableSeeder::class);
     }
 }
