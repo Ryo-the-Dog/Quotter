c19fe0088c721ddfb1992e7c95ffaccbbbf18be9
Ryo-the-Dog <jrt_ryo@yahoo.co.jp>
2020-05-27 17:00:27 +0900

    画像アップロードをcloudinaryに変更中。

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,8 +19,8 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
             //return redirect('/home');
-            return redirect()->route('user'); // ★ 変更
-//            return redirect('/');
+//            return redirect()->route('user'); // ★ 変更
+            return redirect('/');
         }
 
         return $next($request);
